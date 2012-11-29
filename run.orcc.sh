cffdb70e055a704ea00ee65c629a940e2a1154ff
Antoine Lorence <antoine.lorence@gmail.com>
2012-11-29 15:31:19 +0100

    (run) Create output folder if it doesn't exists

--- run.orcc.sh
+++ run.orcc.sh
@@ -5,13 +5,16 @@ source `dirname $0`/defines.sh
 [ -z "$3" ] && echo "Third argument must be the backend you want to use" && exit 1
 [ -z "$4" ] && echo "Fourth argument must be the project where is located your top network" && exit 1
 [ -z "$5" ] && echo "Fifth argument must be the qualified name of the top network" && exit 1
-[ ! -d "$6" ] && echo "Sixth argument must be the directory you want to build sources" && exit 1
+[ -z "$6" ] && echo "Sixth argument must be the directory you want to build sources" && exit 1
+
 APPDIR=$2
 BACKEND=$3
 PROJECT=$4
 NETWORK=$5
 OUTPUT=$6
 FLAGS=$7
+
+[ ! -d "$OUTPUT" ] && mkdir -p $OUTPUT
 
 RUNWORKSPACE=$ORCCWORK/workspace
 mkdir -p $RUNWORKSPACE
