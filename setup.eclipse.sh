566c2cd19290ea061268b8d42e7529a6dc501905
Antoine Lorence <antoine.lorence@gmail.com>
2012-11-30 16:36:06 +0100

    Remove eclipse archive and update script to auto dl it

--- setup.eclipse.sh
+++ setup.eclipse.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source `dirname $0`/defines.sh
 
-BUILDDEPS=org.eclipse.pde.feature.group,org.eclipse.emf.sdk.feature.group,org.eclipse.xtext.sdk.feature.group,org.eclipse.gef.feature.group
-ECLIPSEARCHIVE=eclipse-platform-4.2-linux-gtk-x86_64.tar.gz
+ECLIPSEURL="http://eclipse.ialto.org/eclipse/downloads/drops4/R-4.2.1-201209141800/eclipse-platform-4.2.1-linux-gtk-x86_64.tar.gz"
+BUILDDEPS="org.eclipse.pde.feature.group,org.eclipse.emf.sdk.feature.group,org.eclipse.xtext.sdk.feature.group,org.eclipse.gef.feature.group"
 
 mkdir -p $ORCCWORK
 
@@ -12,12 +12,23 @@ rm -fr $ECLIPSEBUILD
 mkdir $ECLIPSERUN
 mkdir $ECLIPSEBUILD
 
+echo "Downloading Eclipse"
+wget --progress=dot:mega $ECLIPSEURL
+
+ECLIPSEARCHIVE=`echo eclipse-platform-*.tar.gz`
+
+echo "Uncompressing"
 tar -xzaf $ECLIPSEARCHIVE
+
+echo "Copying eclipse/* into $ECLIPSERUN and $ECLIPSEBUILD"
 cp -r eclipse/* $ECLIPSERUN
 cp -r eclipse/* $ECLIPSEBUILD
 
+echo "Deleting 'eclipse' directory and archive downloaded"
 rm -rf eclipse
+rm $ECLIPSEARCHIVE
 
+echo "Installing plugins required for build step into eclipse.build"
 cd $ORCCWORK
 $ECLIPSEBUILD/eclipse 	-nosplash -consoleLog \
 						-application org.eclipse.equinox.p2.director \
