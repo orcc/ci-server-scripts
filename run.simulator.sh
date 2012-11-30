85dc6fe5b9cd12ac0c46657a66ef569b84bbdc1a
Antoine Lorence <antoine.lorence@gmail.com>
2012-11-30 16:02:49 +0100

    Add script to launch cli simulation

---
+++ run.simulator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+source `dirname $0`/defines.sh
+
+[ ! -d "$2" ] && echo "Second argument must be a directory with Orcc projects as subfolders" && exit 1
+[ -z "$3" ] && echo "Third argument must be the project where is located your top network" && exit 1
+[ -z "$4" ] && echo "Fourth argument must be the qualified name of the top network" && exit 1
+[ ! -f "$5" ] && echo "Fifth argument must be the input stimulus of simulation" && exit 1
+[ -f "$6" ] && REFFILE="-r $6"
+
+APPDIR=$2
+PROJECT=$3
+NETWORK=$4
+INPUT=$5
+
+RUNWORKSPACE=$APPDIR
+rm -fr $RUNWORKSPACE/.metadata $RUNWORKSPACE/.JETEmitters 
+rm -fr $RUNWORKSPACE/**/bin
+
+echo "Register Orcc projects in eclipse workspace"
+$ECLIPSERUN/eclipse 	-nosplash -consoleLog \
+						-application net.sf.orcc.cal.workspaceSetup \
+						-data $RUNWORKSPACE \
+						$APPDIR
+
+echo "Generate Orcc IR for $PROJECT and projects it depends on"
+$ECLIPSERUN/eclipse 	-nosplash -consoleLog \
+						-application net.sf.orcc.cal.cli \
+						-data $RUNWORKSPACE \
+						$PROJECT \
+						-vmargs -Xms40m -Xmx768m
+
+echo "Run simulation"
+$ECLIPSERUN/eclipse 	-nosplash -consoleLog \
+						-application net.sf.orcc.simulators.cli \
+						-data $RUNWORKSPACE \
+						-n \
+						-i $INPUT \
+						-p $PROJECT \
+						$NETWORK \
+						-vmargs -Xms40m -Xmx768m
+
+
+exit 0
