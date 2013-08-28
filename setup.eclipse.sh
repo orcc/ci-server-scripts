107bb1b496df55015e564950163947a0ee543f43
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2013-08-28 14:32:50 +0200

    Update eclipse version used (4.2.2 -> 4.3)

--- setup.eclipse.sh
+++ setup.eclipse.sh
@@ -3,7 +3,7 @@ source `dirname $0`/defines.sh
 
 echo "***START*** $0 `date -R`"
 
-ECLIPSEURL="http://eclipse.ialto.com/eclipse/downloads/drops4/R-4.2.2-201302041200/eclipse-platform-4.2.2-linux-gtk-x86_64.tar.gz"
+ECLIPSEURL="http://eclipse.ialto.com/eclipse/downloads/drops4/R-4.3-201306052000/eclipse-platform-4.3-linux-gtk-x86_64.tar.gz"
 
 BUILDDEPS="org.eclipse.pde.feature.group,org.eclipse.emf.sdk.feature.group,org.eclipse.xtext.sdk.feature.group,org.eclipse.gef.feature.group"
 
